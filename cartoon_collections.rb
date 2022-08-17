d3a0a7486ca70470d70b62bb8bc6ab69caf33eb9
Maurice <mauricenganga41@gmail.com>
2022-08-17 12:40:18 +0300

    commit tree 6a9a2fe319e7946934d1c7298678eb138d829135

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
   # Your code here
+  array.map.with_index(1){|name, i| puts "#{i}.#{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  array.map{|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  array.map do |char|
+  if char.length > 4 
+    return true
+  end
+  end
+  return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.map do |item|
+    if cheese_types.include?(item )
+      return item
+    end
+  end
+  nil
 end
+long_planeteer_calls(["Doc", "Dopey", "Bashful", "Grumpy"])
